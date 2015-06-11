22c520a9d4830d218be737c02cc5cd78b15cae2f
hugofisch <marc.burkhardt@gmx.de>
2015-06-11 13:46:25 +0200

    Date Bluetooth and Battery
    Added Date, Battery sign and Bluetooth state sign.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
 #include <pebble.h>
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
+static BitmapLayer *s_battery_layer;
+static GBitmap *s_battery_bitmap;
+static BitmapLayer *s_bluetooth_layer;
+static GBitmap *s_bluetooth_bitmap;
 static TextLayer *s_accel_layer;
+
+static uint8_t s_charge_percent=0;
+static bool s_is_charging=false;
+static bool s_is_plugged=false;
+static bool s_connected=false;
+
 
 static void update_time() {
   // Get a tm structure
@@ -13,6 +24,8 @@ static void update_time() {
   // Create a long-lived buffer
   static char buffer[] = "00:00";
 
+  static char datebuffer[] = "00. Mon";
+
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
     // Use 24 hour format
@@ -21,9 +34,65 @@ static void update_time() {
     // Use 12 hour format
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
+  
+  // Date
+  strftime(datebuffer, sizeof("00. Mon"), "%d. %b", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
+  
+  // Display this time on the TextLayer
+  text_layer_set_text(s_date_layer, datebuffer);
+  
+  if (s_is_charging || s_is_plugged)
+  {
+    gbitmap_destroy(s_battery_bitmap);
+    s_battery_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY_CHARGE);
+    bitmap_layer_set_bitmap(s_battery_layer, s_battery_bitmap);
+  } else {
+    if (s_charge_percent<25)
+    {
+      gbitmap_destroy(s_battery_bitmap);
+      s_battery_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY_EMPTY);
+      bitmap_layer_set_bitmap(s_battery_layer, s_battery_bitmap); 
+    }
+    if ((s_charge_percent>=25)&&(s_charge_percent<50))
+    {
+      gbitmap_destroy(s_battery_bitmap);
+      s_battery_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY_25);
+      bitmap_layer_set_bitmap(s_battery_layer, s_battery_bitmap); 
+    }
+    if ((s_charge_percent>=50)&&(s_charge_percent<75))
+    {
+      gbitmap_destroy(s_battery_bitmap);
+      s_battery_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY_50);
+      bitmap_layer_set_bitmap(s_battery_layer, s_battery_bitmap); 
+    }
+    if ((s_charge_percent>=75)&&(s_charge_percent<100))
+    {
+      gbitmap_destroy(s_battery_bitmap);
+      s_battery_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY_75);
+      bitmap_layer_set_bitmap(s_battery_layer, s_battery_bitmap); 
+    }
+    if ((s_charge_percent==100))
+    {
+      gbitmap_destroy(s_battery_bitmap);
+      s_battery_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY_FULL);
+      bitmap_layer_set_bitmap(s_battery_layer, s_battery_bitmap); 
+    }    
+    if (s_connected)
+    {
+      gbitmap_destroy(s_bluetooth_bitmap);
+      s_bluetooth_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BLUETOOTH_OK);
+      bitmap_layer_set_bitmap(s_bluetooth_layer, s_bluetooth_bitmap); 
+    }
+    else
+    {
+      gbitmap_destroy(s_bluetooth_bitmap);
+      s_bluetooth_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BLUETOOTH_NOTOK);
+      bitmap_layer_set_bitmap(s_bluetooth_layer, s_bluetooth_bitmap);       
+    }
+  }
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -45,31 +114,73 @@ static void accel_tap_handler(AccelAxisType axis, int32_t direction)
     */
 }
 
+static void batterie_handler(BatteryChargeState charge)
+{
+  s_charge_percent=charge.charge_percent;
+  s_is_charging=charge.is_charging;
+  s_is_plugged=charge.is_plugged;
+}
+
+static void bluetooth_handler(bool connected)
+{
+  s_connected=connected;
+}
+
 static void main_window_load(Window *window) {
   //---------------------------
-  // Create GBitmap, then set to created BitmapLayer
+  // Create Background Logo GBitmap, then set to created BitmapLayer
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_LIONS_LOGO2);
   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
   //---------------------------
-    
+
+  
+  //---------------------------
+  // Create Battery GBitmap, then set to created BitmapLayer
+  s_battery_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY_EMPTY);
+  s_battery_layer = bitmap_layer_create(GRect(118, 0, 25, 15));
+  bitmap_layer_set_bitmap(s_battery_layer, s_battery_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_battery_layer));
+  //---------------------------
+  
+  
+  //---------------------------
+  // Create Bluetooth GBitmap, then set to created BitmapLayer
+  s_bluetooth_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BLUETOOTH_NOTOK);
+  s_bluetooth_layer = bitmap_layer_create(GRect(0, 0, 12, 16));
+  bitmap_layer_set_bitmap(s_bluetooth_layer, s_bluetooth_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bluetooth_layer));
+  //---------------------------
+  
   //---------------------------
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(5, 116, 139, 50));
+  s_time_layer = text_layer_create(GRect(5, 118, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
-
   // Improve the layout to be more like a watchface
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
   // Add it as a child layer to the Window's root layer
   layer_set_hidden(text_layer_get_layer(s_time_layer),false);
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   //---------------------------
   
+  
+  //---------------------------
+    // Create date TextLayer
+  s_date_layer = text_layer_create(GRect(5, 148, 139, 18));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_text(s_date_layer, "00. Jan");
+  // Improve the layout to be more like a watchface
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  // Add it as a child layer to the Window's root layer
+  layer_set_hidden(text_layer_get_layer(s_date_layer),false);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+  //---------------------------
    
   //---------------------------
   //Setup Accel Layer
@@ -128,12 +239,18 @@ static void init() {
   window_stack_push(s_main_window, true);
 
   // Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
  
   //Subscribe to AccelerometerService
   accel_service_set_sampling_rate(100);
   accel_tap_service_subscribe(accel_tap_handler);
 
+  //Subscribe BatterieService
+  battery_state_service_subscribe(batterie_handler);
+  
+  //SUbscribe BluetoothService
+   bluetooth_connection_service_subscribe(bluetooth_handler);
+  
   // Make sure the time is displayed from the start
   update_time();
   
